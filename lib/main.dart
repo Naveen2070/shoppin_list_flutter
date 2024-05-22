61fdd74caf47971e9694c38c886cb2f5288f1c17
Naveen2070 <104181086+Naveen2070@users.noreply.github.com>
2024-05-22 18:23:27 +0530

    -added input validationa and input form

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Groceries',
       theme: ThemeData.dark().copyWith(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 147, 229, 250),
+          seedColor: const Color.fromARGB(255, 0, 204, 255),
           brightness: Brightness.dark,
           surface: const Color.fromARGB(255, 42, 51, 59),
         ),
